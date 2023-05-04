00738710936ebe134f0fdd51b1f9b7e080686f77
Тимур <120785772+NetologtTest@users.noreply.github.com>
2023-05-04 18:21:58 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    protected int count = 0;
+    protected Map<String, String> map = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,38 @@
+import org.hamcrest.Matcher;
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        String name = "Имя";
+        String number = "7777777";
+
+        int countTest = phoneBook.add(name, number);
+        Map<String, String> mapTest = new HashMap<>();
+        mapTest = phoneBook.map;
+
+        assertThat(mapTest, IsMapContaining.hasEntry("Имя", "7777777"));
+
+
+        assertEquals(1, countTest);
+
+        phoneBook.add(name, number);
+        mapTest = phoneBook.map;
+        assertThat(mapTest, not(IsMapContaining.hasEntry("Имя", "7777777")));
+    }
+
 }
+
+
