40eb14842637737573097e876d8945db12e0b8e5
Тимур <120785772+NetologtTest@users.noreply.github.com>
2023-05-05 09:13:31 +0300

    findByName implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,25 +2,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    protected int count = 0;
+
     protected Map<String, String> map = new HashMap<>();
 
     public int add(String name, String number) {
-        if (!map.containsKey(name) || !map.containsValue(number)) {
+        if (!map.containsKey(name)) {
 
             map.put(name, number);
-            count++;
         }
 
-        return count;
+        return map.size();
     }
 
     public String findByNumber(String name) {
         return map.get(name);
     }
 
-    public String findByName(String number) {
-        return null;
+
+    public String findByName(String name) {
+        return map.entrySet().stream().filter((x) -> x.getValue().equals(name)).map(Map.Entry::getKey).findFirst().get();
     }
 
 }
